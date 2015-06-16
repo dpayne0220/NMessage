5ac725437f9ccb559330cceebc94fcbafcd5d077
Daniel Payne <daniel.payne.unlimited@gmail.com>
2015-06-16 11:22:44 -0700

    added delete

--- routes/messages.js
+++ routes/messages.js
@@ -2,26 +2,39 @@ var Message = require('../models/message');
 var express = require('express');
 var router = express.Router();
 
-router.get('/message', function(req, res, next) {
-  Message.find(function(err, messages) {
-    if (err) {
-      return res.send(err);
-    }
+router.route('/message')
+  .get(function(req, res, next) {
+      Message.find(function(err, messages) {
+        if (err) {
+          return res.send(err);
+      }
  
-    res.json(messages);
-  });
-});
+      res.json(messages);
+    });
+  })
+  .post(function(req, res, next) {
+    var message = new Message(req.body);
+   
+        message.save(function(err) {
+          if (err) {
+            return res.send(err);
+          }
+   
+        res.send({ message: 'Message Sent' });
+      });
+    });
 
-router.post('/message', function(req, res, next) {
-  var message = new Message(req.body);
- 
-  message.save(function(err) {
-    if (err) {
-      return res.send(err);
-    }
- 
-    res.send({ message: 'Message Sent' });
+router.route('/message/:id')
+  .delete(function(req, res) {
+    Message.remove({
+          _id: req.params.id
+        }, function(err) {
+          if (err) 
+            res.send(err);
+          
+
+      res.send({ message: 'Message Deleted' });
+    });
   });
-});
 
 module.exports = router;
